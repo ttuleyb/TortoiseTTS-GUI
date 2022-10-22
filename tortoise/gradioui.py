d2ef88889a9f617eaa4766941b5e63c9a714b979
Tuleyb <74624982+ttuleyb@users.noreply.github.com>
2022-10-23 00:57:16 +0100

    Update gradioui.py

--- tortoise/gradioui.py
+++ tortoise/gradioui.py
@@ -88,4 +88,5 @@ tripleOutput = gr.Interface(
 demo = tripleOutput
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", share=True) #secret, 44IQUFmIQWOAZGJ
+    #Remove server_name="0.0.0.0" if you don't want to share with your entire wifi network
+    demo.launch(server_name="0.0.0.0", share=False) #Keep share false unless you disable longform or fix the arbitary code execution
