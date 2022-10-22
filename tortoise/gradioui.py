2bac4a0404e52235c570b4165a89b065ed4cc5a0
ttuleyb <simsektuleyb@gmail.com>
2022-10-23 00:37:46 +0100

    Added long form mode

--- tortoise/gradioui.py
+++ tortoise/gradioui.py
@@ -1,37 +1,62 @@
-from symbol import single_input
+import os
 import gradio as gr
-import utils
-import api
+# import utils
+# import api
 import scipy
-from api import *
-import subprocess
-import tortoise_api
+# import tortoise_api
 from tortoise_api import convert_text_to_speech
+import torch
+
+#Migrate API to a seperate script and use cls to access it for easy unloading
+
+if not os.path.exists("desired_outputs/longform"):
+    os.makedirs("desired_outputs/longform")
 
 presetSymbols = {'ultra fast':'ultra_fast', 'fast':'fast', 'standard':'standard', 'high quality': 'high_quality'}
-characterSymbols = {"Sam Harris": "SamH", "Jordan Peterson": "JordanP", "Donald Trump": "DTrump", "Kurzegesagt": "Kurzeg", "Adam Something": "AdamSo", "Bad Empanada": "BadEmp", "Ben Shapiro": "BenS", "Boris Johnson": "BorisJ", "Dennis Prager": "DennisP", "Freeman": "freeman", "Rick Sanchez": "RickS", "Two Minute Papers": "Two Min"}
+characterSymbols = {"Sam Harris": "SamH", "Jordan Peterson": "JordanP", "Donald Trump": "DTrump", "Kurzegesagt": "Kurzeg", "Adam Something": "AdamSo", "Bad Empanada": "BadEmp", "Ben Shapiro": "BenS", "Boris Johnson": "BorisJ", "Dennis Prager": "DennisP", "Freeman": "freeman", "Rick Sanchez": "RickS", "Two Minute Papers": "TwoMin"}
 
-def text_to_speech(text, voice, preset, numOfOutputs):
+def fileFormatter(files):
+    #Read each file and format to work with gradioUI
+    for filen in range(len(files)): #Read all data's from the files
+        files[filen] = scipy.io.wavfile.read(files[filen])
+
+    if len(files) < 3: #Fill empty files to ensure fixed output size
+      while len(files) < 3:
+          files.append("desired_outputs\\empty.wav") #Add empty file to avoid list out of index error
+
+    return files
+
+def text_to_speech(text, voice, preset, readMode, numOfOutputs):
     #Use dictionary to convert strings into symbols readable by the api
     voiceSymbol = characterSymbols[voice]
     presetSymbol = presetSymbols[preset]
 
+    if readMode == "longform":
+        torch.cuda.empty_cache()
+        with open("temporary_text.txt", "w") as f:
+            f.write(text)
+
+        arguments = f"--voice {voiceSymbol} --preset {presetSymbol} --textfile temporary_text.txt --output_path desired_outputs\\longform"
+
+        files = [f"desired_outputs\\longform\\{voiceSymbol}\\combined.wav"]
+
+        os.system(f"python tortoise\\read.py {arguments}")
+
+        #format files
+        files = fileFormatter(files)
+
+        return files
+
     #Create and get files
     files = convert_text_to_speech(text, voiceSymbol, presetSymbol, numOfOutputs)
 
-    #Read each file
-    for filen in range(len(files)):
-        files[filen] = scipy.io.wavfile.read(files[filen])
-
-    if len(files) < 3:
-      while len(files) < 3:
-          files.append("desired_outputs\\empty.wav") #Add empty file to avoid list out of index error
-
-    print(files)
+    #format files
+    files = fileFormatter(files)
 
     return files
 
 tripleOutput = gr.Interface(
+    
     fn = text_to_speech, #Function
     inputs = [ #Inputs
         gr.Textbox(
@@ -46,14 +71,17 @@ tripleOutput = gr.Interface(
 
         gr.Radio(["ultra fast", "fast", "standard", "high quality"], value="fast", label="Speed"),
 
+        gr.Radio(["sentence", "longform"], value="sentence", label="Reading Mode"),
+
         gr.Slider(1, 3, value=3, step=1, label="How many generations do you want?"),
     ],
     outputs = ["audio", "audio", "audio"], #Outputs
     examples=[
-        ["I tell them hell yeah! America is great", "Donald Trump", "fast", 3],
-        ["The universe's third eye is visible when you really look for it", "Sam Harris", "fast", 2],
-        ["The woke liberals are overrunning our university campuses", "Ben Shapiro", "fast", 3],
-        ["Liz Truss, absolute wanker", "Boris Johnson", "fast", 3]
+        ["I tell them hell yeah! America is great", "Donald Trump", "fast", "sentence", 3],
+        ["The universe's third eye is visible when you really look for it", "Sam Harris", "fast","sentence", 2],
+        ["The woke liberals are overrunning our university campuses", "Ben Shapiro","sentence", "fast", 3],
+        ["Liz Truss, absolute wanker", "Boris Johnson", "fast", "sentence", 3],
+        ["Hello dear scholars, Today I'm showing the new sentient AI", "Two Minute Papers", "fast", "longform", 1]
     ],
 )
 
--- tortoise/tortoise_api.py
+++ tortoise/tortoise_api.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import api
-from tortoise import utils
+import tortoise
 import torchaudio
 import os
 import random
@@ -9,7 +9,7 @@ if not os.path.exists("desired_outputs"):
     os.makedirs("desired_outputs")
 
 #Cache things for faster access
-characterSymbols = {"Sam Harris": "SamH", "Jordan Peterson": "JordanP", "Donald Trump": "DTrump", "Kurzegesagt": "Kurzeg", "Adam Something": "AdamSo", "Bad Empanada": "BadEmp", "Ben Shapiro": "BenS", "Boris Johnson": "BorisJ", "Dennis Prager": "DennisP", "Freeman": "freeman", "Rick Sanchez": "RickS", "Two Minute Papers": "Two Min"}
+characterSymbols = {"Sam Harris": "SamH", "Jordan Peterson": "JordanP", "Donald Trump": "DTrump", "Kurzegesagt": "Kurzeg", "Adam Something": "AdamSo", "Bad Empanada": "BadEmp", "Ben Shapiro": "BenS", "Boris Johnson": "BorisJ", "Dennis Prager": "DennisP", "Freeman": "freeman", "Rick Sanchez": "RickS", "Two Minute Papers": "TwoMin"}
 characters = dict.values(characterSymbols)
 
 characterCachedVoices = {}
@@ -17,16 +17,16 @@ characterCachedVoices = {}
 #Load each listed characters voice into memory
 for character in characters:
     clips_paths = Path(f"tortoise/voices/{character}").glob("**/*.wav")
-    reference_clips = [utils.audio.load_audio(p.absolute().__str__(), 22050) for p in clips_paths]
+    reference_clips = [tortoise.utils.audio.load_audio(p.absolute().__str__(), 22050) for p in clips_paths]
     characterCachedVoices[character] = reference_clips
 
-#Initialise tts api
-tts = api.TextToSpeech()
 print("Done caching voices")
 
 def convert_text_to_speech(text, voice, preset, numOfOutputs):    
     reference_clips = characterCachedVoices[voice] #Immediately load voice samples from RAM
 
+    #Initialise tts api
+    tts = api.TextToSpeech()
     gen = tts.tts_with_preset(text, voice_samples=reference_clips, preset=preset, k=numOfOutputs) #Generate speech
 
     randomNum = str(random.randint(1,1000000))
@@ -42,4 +42,8 @@ def convert_text_to_speech(text, voice, preset, numOfOutputs):
         torchaudio.save(filename, gen.squeeze(0).cpu(), 24000)
         files.append(filename)
 
+    del gen
+
     return files
+
+print("Loaded middleman")
